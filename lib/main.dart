739fef4cf8bb54f1575d7c3656b46f8e42711f5e
RufeAmpersand <97739788+RufeAmpersand@users.noreply.github.com>
2025-10-19 21:27:28 +0900

    途中保存

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:megidle/view/main_widget.dart';
 
 void main() {
   runApp(
@@ -15,20 +16,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MainWidget(),
-    );
-  }
-}
-
-class MainWidget extends StatelessWidget {
-  const MainWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
-      ),
+      home: MainPage(),
+      title: 'MegiGuessr',
     );
   }
 }
