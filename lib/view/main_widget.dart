a7f9b0799b23f342680b448e8749889c9e545302
RufeAmpersand <97739788+RufeAmpersand@users.noreply.github.com>
2025-10-19 23:43:28 +0900

    autorouterの削除

--- lib/view/main_widget.dart
+++ lib/view/main_widget.dart
@@ -1,16 +1,10 @@
-import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 
-@RoutePage()
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
-      ),
-    );
+    return Scaffold(body: Center(child: Text('Hello World!')));
   }
 }
