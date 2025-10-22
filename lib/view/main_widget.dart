1176c86870727eb54257704385a937f118b7b026
RufeAmpersand <97739788+RufeAmpersand@users.noreply.github.com>
2025-10-22 23:24:49 +0900

    一時保存

--- lib/view/main_widget.dart
+++ lib/view/main_widget.dart
@@ -1,10 +1,31 @@
 import "package:flutter/material.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
 
-class MainPage extends StatelessWidget {
+class MainPage extends HookConsumerWidget {
   const MainPage({super.key});
-
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Hello World!")));
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.deepPurpleAccent),
+      body: Container(
+        padding: EdgeInsets.all(5),
+        color: Colors.amber,
+        child: Column(
+          spacing: 20,
+          children: [
+            Text(
+              "MegiGuessr",
+              style: TextStyle(
+                color: Colors.deepPurple,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text("表示される様々な情報からメギドの名前を当てましょう！"),
+            ElevatedButton(onPressed: () {}, child: Text("始める")),
+          ],
+        ),
+      ),
+    );
   }
 }
