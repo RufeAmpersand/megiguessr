008b8c7abcc50e72cb42450057555c4f3c611a52
RufeAmpersand <97739788+RufeAmpersand@users.noreply.github.com>
2025-10-22 23:34:15 +0900

    初期画面の配置の仮決定

--- lib/view/main_widget.dart
+++ lib/view/main_widget.dart
@@ -7,23 +7,25 @@ class MainPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.deepPurpleAccent),
-      body: Container(
-        padding: EdgeInsets.all(5),
-        color: Colors.amber,
-        child: Column(
-          spacing: 20,
-          children: [
-            Text(
-              "MegiGuessr",
-              style: TextStyle(
-                color: Colors.deepPurple,
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(5),
+          child: Column(
+            spacing: 20,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "MegiGuessr",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Text("表示される様々な情報からメギドの名前を当てましょう！"),
-            ElevatedButton(onPressed: () {}, child: Text("始める")),
-          ],
+              Text("表示される様々な情報からメギドの名前を当てましょう！"),
+              ElevatedButton(onPressed: () {}, child: Text("始める")),
+            ],
+          ),
         ),
       ),
     );
