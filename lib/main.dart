d3a13e4a2fd75a76b3f68b472ccf524221142421
RufeAmpersand <97739788+RufeAmpersand@users.noreply.github.com>
2026-06-14 14:51:30 +0900

    カラースキームの変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:megiguessr/router/app_router.dart";
+import "package:shadcn_ui/shadcn_ui.dart";
 
 void main() {
   runApp(const ProviderScope(child: MainApp()));
@@ -11,8 +12,22 @@ class MainApp extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp.router(
-      routerConfig: ref.watch(appRouterProvider).config(),
+    return ShadApp.custom(
+      themeMode: ThemeMode.dark,
+      darkTheme: ShadThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ShadVioletColorScheme.dark(),
+      ),
+      appBuilder: (context) {
+        return MaterialApp.router(
+          theme: Theme.of(context),
+          localizationsDelegates: const [GlobalShadLocalizations.delegate],
+          builder: (context, child) {
+            return ShadAppBuilder(child: child);
+          },
+          routerConfig: ref.watch(appRouterProvider).config(),
+        );
+      },
     );
   }
 }
